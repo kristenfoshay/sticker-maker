14793d51e810ca45ab8407a0bf8768c04dc251ec
kristenfoshay <88470531+kristenfoshay@users.noreply.github.com>
2024-11-16 10:37:02 -0500

    Create process_one_sticker.py

---
+++ process_one_sticker.py
@@ -0,0 +1,9 @@
+from sticker_maker import create_sticker_outline
+
+create_sticker_outline(
+    'your_image.png',  
+    'sticker_output.png', 
+    outline_width=20,      
+    outline_color='white',
+    padding=40           
+)
